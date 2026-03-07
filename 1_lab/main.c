0322e6e16493cc43750b2df13d618de95534e895
ISZenzi <zenzirovi@gmail.com>
2026-03-07 14:32:28 +0300

    update CLI

--- 1_lab/main.c
+++ 1_lab/main.c
@@ -2,18 +2,21 @@
 #include <stdlib.h>
 #include "tree.h"
 // спросить про импортирование файлов
+// запуск тестов это всегда отдельный таргет, который не зависит от таргета для запуска программы
 void print_menu()
 {
     printf("Menu:\n");
     printf("1. Insert node\n");
     printf("2. Print tree\n");
-    printf("3. Exit\n");
+    printf("3. Delete node\n");
+    printf("4. Check range\n");
+    printf("5. Exit\n");
 }
 
 int main()
 {
     node *root = NULL;
-    int data, command;
+    int data, command, min, max;
 
     while (1)
     {
@@ -32,9 +35,29 @@ int main()
             tree_print(root, 0);
             break;
         case 3:
+            printf("Enter data ");
+            scanf("%d", &data);
+            root = delete_node(root, data);
+            break;
+        case 4:
+            printf("Enter lower border ");
+            scanf("%d", &min);
+            printf("Enter upper border ");
+            scanf("%d", &max);
+            if (check_range(root, min, max))
+            {
+                printf("ALL leaves in range [%d, %d]\n", min, max);
+            }
+            else
+            {
+                printf("NOT ALL leaves in range [%d, %d]\n", min, max);
+            }
+            break;
+        case 5:
             tree_free(root);
             exit(0);
         default:
+            printf("Invalid command\n");
             break;
         }
     }
