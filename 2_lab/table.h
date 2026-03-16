c2e9021eb6dbb2f089c803cfbddeec64cc57e4c7
ISZenzi <zenzirovi@gmail.com>
2026-03-17 00:42:15 +0300

    update table.h

--- 2_lab/table.h
+++ 2_lab/table.h
@@ -14,12 +14,12 @@ typedef struct
     char *value;
 } MyStruct;
 
-MyStruct *create_table(int size);
 void free_table(MyStruct *table, int size);
-MyStruct *read_table(int *size);
+char *read_line(FILE *file);
+int read_table(MyStruct *table, int limit, const char *filename);
 void print_table(const MyStruct *table, int size);
 void shell_sort(MyStruct *table, int size);
 int binary_search(const MyStruct *table, int size, double key);
-void swap_records(MyStruct *a, MyStruct *b);
+void swap(MyStruct *a, MyStruct *b);
 
 #endif
