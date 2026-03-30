904e722e74af31171cc2d0f35c3e1c7774cea084
ISZenzi <zenzirovi@gmail.com>
2026-03-30 23:00:37 +0300

    add tree reduction and printing funcs

--- 3_lab/tree.c
+++ 3_lab/tree.c
@@ -224,3 +224,126 @@ expr_node *parse_expression(const char *str)
     free(rpn);
     return root;
 }
+
+static expr_node *copy_tree(expr_node *root)
+{
+    if (!root)
+        return NULL;
+
+    expr_node *new_node = (expr_node *)malloc(sizeof(expr_node));
+    if (!new_node)
+        return NULL;
+
+    new_node->type = root->type;
+    new_node->data = root->data;
+    new_node->left = copy_tree(root->left);
+    new_node->right = copy_tree(root->right);
+
+    return new_node;
+}
+
+expr_node *reduce_multiplication(expr_node *root)
+{
+    if (!root)
+        return NULL;
+
+    root->left = reduce_multiplication(root->left);
+    root->right = reduce_multiplication(root->right);
+
+    if (root->type == NODE_OP && root->data.op == '*')
+    {
+        expr_node *expr = NULL;
+        int n = 0;
+
+        if (root->right && root->right->type == NODE_NUM && root->right->data.num > 0)
+        {
+            n = root->right->data.num;
+            expr = root->left;
+        }
+        else if (root->left && root->left->type == NODE_NUM && root->left->data.num > 0)
+        {
+            n = root->left->data.num;
+            expr = root->right;
+        }
+
+        if (expr && n > 0)
+        {
+            if (n == 1)
+            {
+                expr_node *new_root = copy_tree(expr);
+                free_tree(root);
+                return new_root;
+            }
+
+            expr_node *sum = create_op_node('+', copy_tree(expr), copy_tree(expr));
+            for (int i = 2; i < n; i++)
+            {
+                sum = create_op_node('+', sum, copy_tree(expr));
+            }
+
+            free_tree(root);
+            return sum;
+        }
+    }
+
+    return root;
+}
+
+void print_infix(expr_node *root)
+{
+    if (!root)
+        return;
+
+    if (root->type == NODE_OP)
+    {
+        if (root->left == NULL)
+        {
+            if (root->data.op == '-')
+                printf("-");
+            else if (root->data.op == '+')
+                printf("+");
+            print_infix(root->right);
+        }
+        else
+        {
+            printf("(");
+            print_infix(root->left);
+            printf(" %c ", root->data.op);
+            print_infix(root->right);
+            printf(")");
+        }
+    }
+    else if (root->type == NODE_VAR)
+    {
+        printf("%c", root->data.var);
+    }
+    else if (root->type == NODE_NUM)
+    {
+        printf("%d", root->data.num);
+    }
+}
+
+void print_tree_visual(expr_node *root, int depth)
+{
+    if (!root)
+        return;
+
+    print_tree_visual(root->right, depth + 1);
+
+    for (int i = 0; i < depth; i++)
+        printf("    ");
+
+    if (root->type == NODE_OP)
+    {
+        if (root->left == NULL)
+            printf("u%c\n", root->data.op);
+        else
+            printf("%c\n", root->data.op);
+    }
+    else if (root->type == NODE_VAR)
+        printf("%c\n", root->data.var);
+    else if (root->type == NODE_NUM)
+        printf("%d\n", root->data.num);
+
+    print_tree_visual(root->left, depth + 1);
+}
